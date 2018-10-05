76acb3f4d2cb572b2813563d500090d9a7614f01
rakoczUCLA <39495768+rakoczUCLA@users.noreply.github.com>
2018-10-05 10:42:19 -0700

    added exponential increase to lr_find

--- structuredDNN.py
+++ structuredDNN.py
@@ -265,46 +265,138 @@ class SGDRScheduler(Callback):
 #================================================================
 
 
-class LRFinder(Callback):
-    
-    '''
-    A simple callback for finding the optimal learning rate range for your model + dataset. 
-    
+class SGDRScheduler(Callback):
+    '''Cosine annealing learning rate scheduler with periodic restarts.
     # Usage
         ```python
-            lr_finder = LRFinder(min_lr=1e-5, 
-                                 max_lr=1e-2, 
-                                 steps_per_epoch=np.ceil(epoch_size/batch_size), 
-                                 epochs=3)
-            model.fit(X_train, Y_train, callbacks=[lr_finder])
-            
-            lr_finder.plot_loss()
+            schedule = SGDRScheduler(min_lr=1e-5,
+                                     max_lr=1e-2,
+                                     steps_per_epoch=np.ceil(epoch_size/batch_size),
+                                     lr_decay=0.9,
+                                     cycle_length=5,
+                                     mult_factor=1.5)
+            model.fit(X_train, Y_train, epochs=100, callbacks=[schedule])
         ```
-    
     # Arguments
         min_lr: The lower bound of the learning rate range for the experiment.
         max_lr: The upper bound of the learning rate range for the experiment.
         steps_per_epoch: Number of mini-batches in the dataset. Calculated as `np.ceil(epoch_size/batch_size)`. 
-        epochs: Number of epochs to run experiment. Usually between 2 and 4 epochs is sufficient. 
+        lr_decay: Reduce the max_lr after the completion of each cycle.
+                  Ex. To reduce the max_lr by 20% after each cycle, set this value to 0.8.
+        cycle_length: Initial number of epochs in a cycle.
+        mult_factor: Scale epochs_to_restart after each full cycle completion.
+    # References
+        Blog post: jeremyjordan.me/nn-learning-rate
+        Original paper: http://arxiv.org/abs/1608.03983
+    '''
+    def __init__(self,
+                 min_lr,
+                 max_lr,
+                 batch_size,
+                 tr_sample_size,
+                 lr_decay=1,
+                 cycle_length=10,
+                 mult_factor=2):
+
+        self.min_lr = min_lr
+        self.max_lr = max_lr
+        self.lr_decay = lr_decay
+
+        self.batch_since_restart = 0
+        self.next_restart = cycle_length
+
+        self.steps_per_epoch = np.ceil(tr_sample_size/batch_size)
+
+        self.cycle_length = cycle_length
+        self.mult_factor = mult_factor
+
+        self.history = {}
+
+    def clr(self):
+        '''Calculate the learning rate.'''
+        fraction_to_restart = self.batch_since_restart / (self.steps_per_epoch * self.cycle_length)
+        lr = self.min_lr + 0.5 * (self.max_lr - self.min_lr) * (1 + np.cos(fraction_to_restart * np.pi))
+        return lr
+
+    def on_train_begin(self, logs={}):
+        '''Initialize the learning rate to the minimum value at the start of training.'''
+        logs = logs or {}
+        K.set_value(self.model.optimizer.lr, self.max_lr)
+
+    def on_batch_end(self, batch, logs={}):
+        '''Record previous batch statistics and update the learning rate.'''
+        logs = logs or {}
+        self.history.setdefault('lr', []).append(K.get_value(self.model.optimizer.lr))
+        for k, v in logs.items():
+            self.history.setdefault(k, []).append(v)
+
+        self.batch_since_restart += 1
+        K.set_value(self.model.optimizer.lr, self.clr())
+
+    def on_epoch_end(self, epoch, logs={}):
+        '''Check for end of current cycle, apply restarts when necessary.'''
+        if epoch + 1 == self.next_restart:
+            self.batch_since_restart = 0
+            self.cycle_length = np.ceil(self.cycle_length * self.mult_factor)
+            self.next_restart += self.cycle_length
+            self.max_lr *= self.lr_decay
+            self.best_weights = self.model.get_weights()
+
+#     def on_train_end(self, logs={}):
+#         '''Set weights to the values from the end of the most recent cycle for best performance.'''
+#         self.model.set_weights(self.best_weights)
+ 
+class LRFinder(Callback):
+    
+    '''
+    A callback for finding the optimal learning rate range for your model + dataset. 
+    
+    # Usage
+        mixedInputModel.lr_finder()
+        mixedInputModel.lr_finder.plot_lr()
+    
+    # Arguments
+        min_lr: The lower bound of the learning rate range for the experiment.
+        max_lr: The upper bound of the learning rate range for the experiment.
+        tr_sample_size: number of asmples in data-set
+        epochs: Number of epochs to run experiment. Usually 1. 
         
     # References
         Blog post: jeremyjordan.me/nn-learning-rate
         Original paper: https://arxiv.org/abs/1506.01186
     '''
     
-    def __init__(self, min_lr=1e-5, max_lr=1e-2, steps_per_epoch=None, epochs=None):
+    def __init__(self,
+                 batch_size,
+                 tr_sample_size,
+                 min_lr=1e-5,
+                 max_lr=1.0,
+                 epochs=1,
+                 linear = False):
+        
         super().__init__()
         
         self.min_lr = min_lr
         self.max_lr = max_lr
+        self.batch_size = batch_size
+        self.tr_sample_size = tr_sample_size
+        steps_per_epoch=np.ceil(tr_sample_size/batch_size) # Number of mini-batches in the dataset.
         self.total_iterations = steps_per_epoch * epochs
         self.iteration = 0
         self.history = {}
+        self.linear = linear
+        
+        ratio = max_lr/min_lr
+        self.lr_mult = ratio**(1/steps_per_epoch)
         
     def clr(self):
         '''Calculate the learning rate.'''
-        x = self.iteration / self.total_iterations 
-        return self.min_lr + (self.max_lr-self.min_lr) * x
+        if(self.linear):
+            x = self.iteration / self.total_iterations 
+            return self.min_lr + (self.max_lr-self.min_lr) * x
+        else:
+            mult = self.lr_mult**self.iteration
+            return self.min_lr * mult
         
     def on_train_begin(self, logs=None):
         '''Initialize the learning rate to the minimum value at the start of training.'''
@@ -327,7 +419,7 @@ class LRFinder(Callback):
     def plot_lr(self):
         '''Helper function to quickly inspect the learning rate schedule.'''
         plt.plot(self.history['iterations'], self.history['lr'])
-        plt.yscale('log')
+        #plt.yscale('log')
         plt.xlabel('Iteration')
         plt.ylabel('Learning rate')
         
