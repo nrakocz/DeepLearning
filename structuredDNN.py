ed5197860b9ad33a6653681be31b50765f44defb
rakoczUCLA <39495768+rakoczUCLA@users.noreply.github.com>
2018-10-05 14:19:15 -0700

    Update structuredDNN.py

--- structuredDNN.py
+++ structuredDNN.py
@@ -1,10 +1,3 @@
-from keras.callbacks import Callback
-import keras.backend as K
-import numpy as np
-from keras import layers, models, Model, Input
-import keras
-import matplotlib.pyplot as plt
-
 class mixedInputModel():
 
     def __init__(self,df,y,cat_vars,emb_szs,hidden_layers,hidden_drop,
@@ -54,10 +47,26 @@ class mixedInputModel():
         self.cont_input = layers.Input(shape=(self.cont_df.shape[1],))
         cont = layers.Dropout(self.cont_input_drop)(self.cont_input)
 
+        # SAVE INPUT (TENSORS & DATA)
+        self.input_tensors = [self.cont_input]+self.emb_inputs
+        self.input_data_reform = [self.cont_df]+self.cat_values
+        
         # CONCATENATE CONTINOUS AND EMBEDDED CATEGORICAL
-        x = keras.layers.Concatenate(name='All_features')([ent_emb,cont])
+        self.joint_tensor = keras.layers.Concatenate(name='All_features')([ent_emb,cont])
+        
 
         # DENSE HIDDEN LAYERS
+        self.output_tensor = self.simpleDNN(self.joint_tensor)
+                   
+        # DEFINE AND COMPILE MODEL
+        self.defineAndCompile()
+    
+    
+    
+    def simpleDNN(self,nn_input_tensor):
+        
+        x = layers.Lambda(lambda x:x)(nn_input_tensor)
+        
         for l,dr in zip(self.hidden_layers,self.hidden_drop):
             x = layers.Dense(l,activation=self.hidden_activation,)(x)
             if self.use_bn: x = layers.BatchNormalization()(x)
@@ -69,24 +78,43 @@ class mixedInputModel():
         else:
             self.output_activation = 'sigmoid'
         
-        self.output_tensor = layers.Dense(self.output_size,name='Output_Layer',activation=self.output_activation)(x)        
+        return layers.Dense(self.output_size,name='Output_Layer',activation=self.output_activation)(x)        
         
-            
+    
+    def defineAndCompile(self):
         
-        # DEFINE MODEL
-        self.struct_model = keras.Model(inputs=[self.cont_input]+self.emb_inputs,outputs=self.output_tensor)
-        self.input_tensors = [self.cont_input]+self.emb_inputs
-        
+        self.struct_model = keras.Model(inputs=self.input_tensors,outputs=self.output_tensor)
+               
         # COMPILE MODEL
         self.opt = self.opt if self.opt is not None else keras.optimizers.Adam() # keras.optimizers.SGD(momentum=0.9)
         self.struct_model.compile(optimizer=self.opt, loss=self.loss,metrics=self.metrics)
-
+#         self.init_weights = self.struct_model.get_weights()
+        
+    
+    
+    def addModelToJointFeat(self,new_df,input_tensor,concat_tensor):
+        
+        # APPEND NEW INPUT
+        self.input_tensors.append(input_tensor)
+        self.input_data_reform.append(new_df)
+        
+        # CONCATENATE NEW FEATURES
+        self.joint_tensor = keras.layers.Concatenate(name='new_joint_features')([self.joint_tensor,concat_tensor])
+       
+        # PASS THROUGH DNN
+        self.output_tensor = self.simpleDNN(self.joint_tensor)
+        
+        # DEFINE AND COMPILE
+        self.defineAndCompile()
+        
+    
     def fit(self, lr, batch_size = 128,epochs = 1, val_split = 0.0,validation_data=None,callbacks=None,
-           cycle_length=1,mult_factor=1,sgdr=True):
+           cycle_length=1,mult_factor=1,sgdr=True,save_best=True,model_path=None):
         
         
         callbacks = callbacks if callbacks is not None else []
         
+        # LR SCHEDULE
         schedule = SGDRScheduler(min_lr=lr/10,
                                  max_lr=lr,
                                  batch_size=batch_size,
@@ -96,9 +124,21 @@ class mixedInputModel():
                                  mult_factor=mult_factor)
         if sgdr: callbacks.append(schedule)
         
-        #pdb.set_trace()  
-        log=self.struct_model.fit(x=[self.cont_df]+self.cat_values,y=[self.y]
-                 ,batch_size=batch_size,epochs=epochs,callbacks=callbacks,validation_split=val_split,
+        
+        # SAVE BEST
+        if(save_best):
+            model_path = model_path if model_path is not None else './BestDNN_Model'
+            save_best = keras.callbacks.ModelCheckpoint(model_path, 
+                                                    monitor='val_loss',
+                                                    verbose=0, 
+                                                    save_best_only=True, save_weights_only=False,
+                                                    mode='auto',
+                                                    period=1)
+            callbacks.append(save_best)
+            self.best_model_path=model_path
+
+        log=self.struct_model.fit(x=self.input_data_reform,y=[self.y],
+                 batch_size=batch_size,epochs=epochs,callbacks=callbacks,validation_split=val_split,
                              validation_data=validation_data)
         
         
@@ -110,6 +150,10 @@ class mixedInputModel():
                 
         self.schedule = schedule
             
+    def loadBestModel(self):
+        self.struct_model.load_weights(self.best_model_path)
+        self.defineAndCompile()
+        
     def print_acc(self):
 
         # summarize history for accuracy
@@ -157,8 +201,14 @@ class mixedInputModel():
         plt.title('model lr')
         plt.ylabel('lr')
         plt.xlabel('batch')
-        plt.show()
-   
+        plt.show()   
+        
+# class LRHistory(keras.callbacks.Callback):
+#     def on_train_begin(self, logs={}):
+#         self.lr = []
+
+#     def on_batch_end(self, batch, logs={}):
+#         self.losses.append(logs.get('loss'))
     
     def find_lr(self,min_lr=1e-5,max_lr=1.0,epochs=1,batch_size=256,linear=False):
         
@@ -170,14 +220,17 @@ class mixedInputModel():
                              linear=linear
                             )
         
-        w = self.struct_model.get_weights()
+        
         self.struct_model.fit(x=[self.cont_df]+self.cat_values,y=[self.y],
                               batch_size=batch_size,
                               callbacks=[lr_finder])
-        self.struct_model.set_weights(w)
+        self.struct_model.set_weights(self.init_weights)
 
         lr_finder.plot_loss()
         self.lr_finder = lr_finder
+        
+#================================================================
+#================================================================
 #================================================================
 
 
@@ -193,112 +246,11 @@ def genStructModelInput(df,cat_vars):
 
 #================================================================
 #================================================================
-# Learning Rate schedulers (SGDR & LR-FINDER) 
-# Based on Implementation by Jeremy Jordan. 
-# (https://gist.github.com/jeremyjordan)
-#================================================================
-#================================================================
-class SGDRScheduler(Callback):
-    '''Cosine annealing learning rate scheduler with periodic restarts.
-    # Usage
-        ```python
-            schedule = SGDRScheduler(min_lr=1e-5,
-                                     max_lr=1e-2,
-                                     steps_per_epoch=np.ceil(epoch_size/batch_size),
-                                     lr_decay=0.9,
-                                     cycle_length=5,
-                                     mult_factor=1.5)
-            model.fit(X_train, Y_train, epochs=100, callbacks=[schedule])
-        ```
-    # Arguments
-        min_lr: The lower bound of the learning rate range for the experiment.
-        max_lr: The upper bound of the learning rate range for the experiment.
-        steps_per_epoch: Number of mini-batches in the dataset. Calculated as `np.ceil(epoch_size/batch_size)`. 
-        lr_decay: Reduce the max_lr after the completion of each cycle.
-                  Ex. To reduce the max_lr by 20% after each cycle, set this value to 0.8.
-        cycle_length: Initial number of epochs in a cycle.
-        mult_factor: Scale epochs_to_restart after each full cycle completion.
-    # References
-        Blog post: jeremyjordan.me/nn-learning-rate
-        Original paper: http://arxiv.org/abs/1608.03983
-    '''
-    def __init__(self,
-                 min_lr,
-                 max_lr,
-                 batch_size,
-                 tr_sample_size,
-                 lr_decay=1,
-                 cycle_length=10,
-                 mult_factor=2):
-
-        self.min_lr = min_lr
-        self.max_lr = max_lr
-        self.lr_decay = lr_decay
-
-        self.batch_since_restart = 0
-        self.next_restart = cycle_length
-
-        self.steps_per_epoch = np.ceil(tr_sample_size/batch_size)
-
-        self.cycle_length = cycle_length
-        self.mult_factor = mult_factor
-
-        self.history = {}
-
-    def clr(self):
-        '''Calculate the learning rate.'''
-        fraction_to_restart = self.batch_since_restart / (self.steps_per_epoch * self.cycle_length)
-        lr = self.min_lr + 0.5 * (self.max_lr - self.min_lr) * (1 + np.cos(fraction_to_restart * np.pi))
-        return lr
-
-    def on_train_begin(self, logs={}):
-        '''Initialize the learning rate to the minimum value at the start of training.'''
-        logs = logs or {}
-        K.set_value(self.model.optimizer.lr, self.max_lr)
-
-    def on_batch_end(self, batch, logs={}):
-        '''Record previous batch statistics and update the learning rate.'''
-        logs = logs or {}
-        self.history.setdefault('lr', []).append(K.get_value(self.model.optimizer.lr))
-        for k, v in logs.items():
-            self.history.setdefault(k, []).append(v)
-
-        self.batch_since_restart += 1
-        K.set_value(self.model.optimizer.lr, self.clr())
-
-    def on_epoch_end(self, epoch, logs={}):
-        '''Check for end of current cycle, apply restarts when necessary.'''
-        if epoch + 1 == self.next_restart:
-            self.batch_since_restart = 0
-            self.cycle_length = np.ceil(self.cycle_length * self.mult_factor)
-            self.next_restart += self.cycle_length
-            self.max_lr *= self.lr_decay
-            self.best_weights = self.model.get_weights()
-
-#     def on_train_end(self, logs={}):
-#         '''Set weights to the values from the end of the most recent cycle for best performance.'''
-#         self.model.set_weights(self.best_weights)
-
-def lr_finder(self,min_lr=1e-5,max_lr=1.0,epochs=1,batch_size=256):
-        
-        lr_finder = LRFinder(min_lr=min_lr, 
-                             max_lr=max_lr,                               
-                             epochs=epochs,
-                             batch_size=batch_size,
-                             tr_sample_size = np.ceil(self.df.shape[0])
-                            )
-        
-        
-        self.struct_model.fit(x=[self.cont_df]+self.cat_values,y=[self.y],
-                              batch_size=batch_size,
-                              callbacks=[lr_finder])
-
-        lr_finder.plot_loss()
-        self.lr_finder = lr_finder
-#================================================================
-#================================================================
 #================================================================
 
+from keras.callbacks import Callback
+import keras.backend as K
+import numpy as np
 
 class SGDRScheduler(Callback):
     '''Cosine annealing learning rate scheduler with periodic restarts.
@@ -384,17 +336,24 @@ class SGDRScheduler(Callback):
 class LRFinder(Callback):
     
     '''
-    A callback for finding the optimal learning rate range for your model + dataset. 
+    A simple callback for finding the optimal learning rate range for your model + dataset. 
     
     # Usage
-        mixedInputModel.lr_finder()
-        mixedInputModel.lr_finder.plot_lr()
+        ```python
+            lr_finder = LRFinder(min_lr=1e-5, 
+                                 max_lr=1e-2, 
+                                 steps_per_epoch=np.ceil(epoch_size/batch_size), 
+                                 epochs=3)
+            model.fit(X_train, Y_train, callbacks=[lr_finder])
+            
+            lr_finder.plot_loss()
+        ```
     
     # Arguments
         min_lr: The lower bound of the learning rate range for the experiment.
         max_lr: The upper bound of the learning rate range for the experiment.
-        tr_sample_size: number of asmples in data-set
-        epochs: Number of epochs to run experiment. Usually 1. 
+        steps_per_epoch: Number of mini-batches in the dataset. Calculated as `np.ceil(epoch_size/batch_size)`. 
+        epochs: Number of epochs to run experiment. Usually between 2 and 4 epochs is sufficient. 
         
     # References
         Blog post: jeremyjordan.me/nn-learning-rate
@@ -415,7 +374,7 @@ class LRFinder(Callback):
         self.max_lr = max_lr
         self.batch_size = batch_size
         self.tr_sample_size = tr_sample_size
-        steps_per_epoch=np.ceil(tr_sample_size/batch_size) # Number of mini-batches in the dataset.
+        steps_per_epoch=np.ceil(tr_sample_size/batch_size)
         self.total_iterations = steps_per_epoch * epochs
         self.iteration = 0
         self.history = {}
@@ -464,7 +423,17 @@ class LRFinder(Callback):
         plt.xscale('log')
         plt.xlabel('Learning rate')
         plt.ylabel('Loss')
+
+def createSimpleDNN(df,hidden_layers,hidden_drop,output_size,output_activation,input_drop=0,hidden_activation='relu',use_bn=True):
         
-#================================================================
-#================================================================
-#================================================================
+        input_tensor = layers.Input(shape=(df.shape[1],))
+        x = layers.Dropout(input_drop)(input_tensor)
+        
+        for l,dr in zip(hidden_layers,hidden_drop):
+            x = layers.Dense(l,activation=hidden_activation,)(x)
+            if use_bn: x = layers.BatchNormalization()(x)
+            x = layers.Dropout(dr)(x)
+                
+        output_tensor = layers.Dense(output_size,activation=output_activation)(x)
+        
+        return input_tensor,output_tensor
